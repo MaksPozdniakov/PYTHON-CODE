953f6ce9bfcc8e7e9906e0121ee30f2f372b4391
Maksym Pozdniakov <makpoz1@st.amu.edu.pl>
2022-11-21 17:34:46 +0100

    2 commit

---
+++ ITCSP/ITCSP_01.py
@@ -0,0 +1,29 @@
+print('Hello World')
+print('I live in Poznan')
+
+print('Ana has a cat', end='')
+print('and a dog')
+
+# print('Hello')
+print('Hello')
+
+age = 35
+Iam = "I am"
+years = "years old"
+print(Iam, age, years)
+
+print("Static program, ver.1.002")
+
+type(3.1)
+print(type(3.1))
+
+float(3)
+
+x = 5
+print(x+6)
+y = x+6
+print(y)
+
+float(3)
+int(3.09)
+print(float, int)
