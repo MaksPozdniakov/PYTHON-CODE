9025d5985570db751ea86bbecde546922f9ec9b5
Maksym Pozdniakov <makpoz1@st.amu.edu.pl>
2023-04-01 20:42:15 +0200

    class 5

---
+++ ITCSP/class_pp_05.py
@@ -0,0 +1,21 @@
+# multiplication iteration
+# def mult_iter(a, b):
+#     result = 0
+#     while b > 0:
+#         result += a
+#         b -= 1
+#     return result
+
+# a = int(input('a = '))
+# b = int(input('b = '))
+# print(f'{a} * {b} = {mult_iter(a,b)}')
+
+a, b = 11, 15
+min = a if a < b else b
+print(min)
+
+for i in range(1, 5):
+    pass
+a, b = 11, 15
+if a < b:
+    pass
